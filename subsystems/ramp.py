1d72727f3d8d4032c18266cddd1e17f7e2aa6556
Robotics4 <cluckeykid121@gmail.com>
2019-02-06 19:17:55 -0500

    Edited ramp.py

--- subsystems/ramp.py
+++ subsystems/ramp.py
@@ -1,15 +1,19 @@
 import math
 import wpilib
 from wpilib.command.subsystem import Subsystem
-from wpilib.doublesolenoid import DoubleSolenoid
+from wpilib.solenoid import Solenoid
 from wpilib.digitalinput import DigitalInput
 
 import subsystems
 import robotmap
 
 class Ramp(subsystem):
-    print("Ramp: init called")
-    super().__init__('Ramp')
-    self.logPrefix= 'Ramp: '
+    def __init__(self):
+        print("Ramp: init called")
+        super().__init__('Ramp')
+        self.logPrefix= 'Ramp: '
+        self.rampSolenoid = wpilib.solenoid(1,0,1)
 
-
+    def initDefaultCommand(self):
+        self.setDefaultCommand(RampControls())
+        print("{}Default command set to RampControls".format(self.logPrefix)) 
