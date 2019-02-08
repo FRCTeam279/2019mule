edb354857069bc158dbbc9433896af7e2a83f46e
Cordelia V <cordelia.vanderveer@toledotechnologyacademy.org>
2019-02-07 19:04:36 -0500

    cargo grab subsystems and commands updated, oi.py and robotmap.py edited to reflect

--- subsystems/cargograb.py
+++ subsystems/cargograb.py
@@ -1,5 +1,5 @@
-# Two servos to raise and lower the door that keeps the ball from falling out
-# Contains the cargo
+# Two servos acting as a claw, picking up and carrying cargo
+# Connected to elevator
 import math
 import wpilib
 from wpilib.command.subsystem import Subsystem
@@ -15,9 +15,9 @@ class CargoGrab(Subsystem):
         super().__init__('CargoGrab')
         self.logPrefix = "CargoGrab: "
     
-    self.servo1 = wpilib.PWM(robotmap.cargograb)
+        self.leftservo = wpilib.PWM(robotmap.cargograb)
 
-    self.servo2 = wpilib.PWM(robotmap.cargograb)
+        self.rightservo = wpilib.PWM(robotmap.cargograb)
 #-----------------------------------------------------------------------------------------
 
 
@@ -27,25 +27,12 @@ class CargoGrab(Subsystem):
         print("{}Default command set to CargoGrab".format(self.logPrefix)) 
 '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 
-    def openCargoHold(self):
+def openCargoHold(self):
+    self.leftservo.set(1.0)
+    self.rightservo.set(1.0)
 
-'''
-Michael's Comments  (delete these lines when done)
-Lines 18 and 20 need to be indented so that they are within the __init__(self) function
-
-Instead of naming the servers as server1 and server2, i'd strongly suggest left and right (or top/bottom, front/back, etc..) so that 
-it's clear which physical servo is being referred to.  Now having said that, left/right and all the others 
-assume you're at the back of the robot looking towards the front.... (same as if it was your person)
-
-THe names of variables in robotmap shoudl be updated to match.  something like 
-robotmap.cargograb.rightServoPort 
-
-You'll only need a default command on this subsystem if you want to control the angle of the door with a joystick live, 
-as opposed ot just hitting a button to fully open or close.  Maybe that'd be useful.. i don't know.  Try it if you want 
-as it'd be good experience to play with even if you don't use it on the production robot.
-
-Definitly plan on having a command to open, and a separate command to close bound to separate buttons
-You'll want to store the target angle/setting for each one in the robotmap file
-'''
-
+def closeCargoHold(self):
+    self.leftservo.set(-1.0)
+    self.rightservo.set(-1.0)
+        
 
